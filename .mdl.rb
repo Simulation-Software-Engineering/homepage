4bbee647b72445469547b592f08ed353efd05d82
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-09-21 11:55:59 +0200

    [ci] update markdownlint confing including adapted rules

---
+++ .mdl.rb
@@ -0,0 +1,14 @@
+# Enable all rules by default
+all
+
+# Extend line length for text.
+# This will complain for overly wide tables and code blocks.
+rule 'MD013', :line_length => 99999
+
+exclude_rule 'MD012'
+
+exclude_rule 'MD040'
+
+# Nested lists should be indented with four spaces.
+# Modification: Question marks should be allowed
+rule 'MD26', :punctuation => '.,;:!'
