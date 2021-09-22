45bc8d1c6666e68af5a5be9e68513f804474b350
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-09-22 12:32:36 +0200

    [ci] update markdownlint config
    Update removes the requirements regarding top-level headers as these are set by the metadata extension

--- .mdl.rb
+++ .mdl.rb
@@ -5,6 +5,12 @@ all
 # This will complain for overly wide tables and code blocks.
 rule 'MD013', :line_length => 99999
 
+# Allow first header to not be a top level header
+# since top-level is set by metadata
+exclude_rule 'MD002'
+# First line in file does not have to be a top level header
+# since top-level is set by metadata
+exclude_rule 'MD041'
 # Allow multiple consecutive blank lines
 exclude_rule 'MD012'
 # Allow raw HTML to include videos, for example.
